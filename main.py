44d1e90ce569c9a8bdef4b1758e93f40cde4bd11
gegemonTV <vovakonki776@gmail.com>
2021-02-15 01:18:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+token = "ODEwNjI5MjI2NDYxNTI4MDc1.YCmbTA.zaQyhdd9ykeTp1i9ZARMtN2MMa0"
+
+bot = commands.Bot(command_prefix='-')
+
+@bot.command()
+async def play(ctx)
+
+
+bot.run(token)
